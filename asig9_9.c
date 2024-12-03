82d3fe15c8b66b77a71084d3353ed10edd5ac5fc
Shreyanshim <87068610+Shreyanshim@users.noreply.github.com>
2024-12-03 18:50:05 +0530

    Add files via upload

---
+++ asig9_9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            arr[i] -= 1;
+        } else {
+            arr[i] += 1; 
+        }
+    }
+
+    printf("The modified array is:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
